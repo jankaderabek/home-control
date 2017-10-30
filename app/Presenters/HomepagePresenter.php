23b5bbc6313bbb0e97d656a1d09e274999a09d63
Kadeřábek <jan.kaderabek@peckadesign.cz>
2017-10-30 17:41:36 +0100

    Errors into elastic

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -7,4 +7,8 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	public function actionDefault()
+	{
+		\Tracy\Debugger::log('Test' . (new \DateTime())->format('Y-m-d H:i:s'), \Tracy\ILogger::ERROR);
+	}
 }
